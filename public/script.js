a699df414f7a9fd640819d3f2c8a8a147463b13c
AMEDZ <aldo.medina.z@gmail.com>
2019-07-27 12:37:48 +0100

    added semicolons in script.js

--- public/script.js
+++ public/script.js
@@ -8,21 +8,19 @@ if ('geolocation' in navigator) {
             lon = position.coords.longitude;                                  
     
             // Get weather and air quality data from server. The server is been use as a server proxy for an external API, so this fetch (the client-side) is fetching the information gathered from the server's fetch thats feteching from the aq and weather's APi.
-            const api_url = `weather/${lat},${lon}`    
+            const api_url = `weather/${lat},${lon}`;    
             const response = await fetch(api_url);
-            const json = await response.json()     
+            const json = await response.json();     
             
             // Select the data and do UI stuffs
             weather = json.weather.currently;
             aq = json.aq.results[0].measurements[0];            
-            document.getElementById('container').innerText = `Currently, the temperature in your location (${lat.toFixed(2)}º, ${lon.toFixed(2)}º) is ${weather.temperature}º C in a "${weather.summary.toLowerCase()}" day. The air quality is: ${aq.value} ${aq.unit} (measurement's parameter: ${aq.parameter}).`
+            document.getElementById('container').innerText = `Currently, the temperature in your location (${lat.toFixed(2)}º, ${lon.toFixed(2)}º) is ${weather.temperature}º C in a "${weather.summary.toLowerCase()}" day. The air quality is: ${aq.value} ${aq.unit} (measurement's parameter: ${aq.parameter}).`;
             
         } catch(error) {
-            console.error(error)
-            document.getElementById('container').innerText = `Currently, the temperature in your location (${lat.toFixed(2)}º, ${lon.toFixed(2)}º) is ${weather.temperature}º C in a "${weather.summary.toLowerCase()}" day. There are no air quality readings for your current location.`
-            aq = {
-                value: -1
-            }
+            console.error(error);
+            document.getElementById('container').innerText = `Currently, the temperature in your location (${lat.toFixed(2)}º, ${lon.toFixed(2)}º) is ${weather.temperature}º C in a "${weather.summary.toLowerCase()}" day. There are no air quality readings for your current location.`;
+            aq = { value: -1};
         }        
         
         // Sending back with POST-fetch the data that will be store in the database.
